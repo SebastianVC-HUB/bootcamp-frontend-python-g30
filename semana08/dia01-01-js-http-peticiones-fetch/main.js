7bd66372dd46ceca9ef4fbfe0c3a21df34fee241
SebastianVC-HUB <sebastian.vc.0820@gmail.com>
2026-01-19 12:28:52 -0500

    semana08, dia01, javascript http peticiones fetch todos

--- semana08/dia01-01-js-http-peticiones-fetch/main.js
+++ semana08/dia01-01-js-http-peticiones-fetch/main.js
@@ -19,3 +19,35 @@ console.log(JSON.parse('{ "name": "Alonso" }'))
 // OBJETIVO: Traer información en formato JSON desde una URL
 
 const url = "https://jsonplaceholder.typicode.com/todos"
+
+// console.log(fetch(url)) // Devuelve una promesa (Promise)
+
+// Estados de una promesa -> pending (se esta procesando el llamado), fulfilled (ya la información llego) y rejected (la información llego pero con algún problema)
+
+//fetch(url)
+//    .then(response => response.json()) // Se ejecuta cuando la respuesta llego correctamente y además convertimos la respuesta a un objeto JS.
+//    .then(data =>{
+//        console.log(data) // Aquí podemos usar el objeto JS
+//        console.log(data[0].title)
+//    })
+
+const renderTodos = (todos = []) => {
+    const divApp = document.querySelector('#app')
+
+    let todoList = ''
+
+    todos.forEach(todo => {
+        todoList += `<h2>${todo.id} - ${todo.title}</h2>`
+    })
+
+    divApp.innerHTML = todoList
+}
+
+renderTodos()
+
+fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(data => {
+        console.log(data)
+        renderTodos(data)
+    })
