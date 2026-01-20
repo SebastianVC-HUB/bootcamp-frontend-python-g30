691ff465b525658b7e82fc6fdd33b0e88457b865
SebastianVC-HUB <sebastian.vc.0820@gmail.com>
2026-01-20 13:58:42 -0500

    semana08, dia02,javascript fetch pokemons vista de la api con los pokemons

--- semana08/dia02-01-js-fetch-pokemons-paginación/main.js
+++ semana08/dia02-01-js-fetch-pokemons-paginación/main.js
@@ -8,7 +8,8 @@
 //     .then(data => console.log(data))
 
 const fetchPokemons = async () => {
-    const API_URL = 'https://pokeapi.co/api/v2/pokemon'
+    // const API_URL = 'https://pokeapi.co/api/v2/pokemon'
+    const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=6&offset=0'
     const response = await fetch(API_URL) // Devuelve una promesa
 
     const data = await response.json() // Convierte la respuesta en formato JS Object
