fe60535ab897d44b3fa3f1edbd073e0c5e9605b6
SebastianVC-HUB <sebastian.vc.0820@gmail.com>
2026-01-20 12:49:15 -0500

    semana08, dia02,javascript fetch pokemons mostrar la data

--- semana08/dia02-01-js-fetch-pokemons-paginación/main.js
+++ semana08/dia02-01-js-fetch-pokemons-paginación/main.js
@@ -1,2 +1,74 @@
 // TODO: Listar los pokemons en la consola usando la pokeapi
 // https://pokeapi.co/api/v2/pokemon 
+
+ // fetch -> Devuelve una promesa
+
+// fetch('https://pokeapi.co/api/v2/pokemon') 
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+const fetchPokemons = async () => {
+    const API_URL = 'https://pokeapi.co/api/v2/pokemon'
+    const response = await fetch(API_URL) // Devuelve una promesa
+
+    const data = await response.json() // Convierte la respuesta en formato JS Object
+
+    // TODO: Agregar el id a cada pokemon dentro del arreglo results para usarlo en la imagen del pokemon
+
+    const dataResults = data.results.map(pokemon => {
+        const id = pokemon.url.split('/').at(6)
+        const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+
+        return {
+            ...pokemon, //name, url
+            id,
+            image
+        }
+    })
+
+    return {
+        ...data, // count, next, previous, results
+        results: dataResults
+    }
+}
+
+//Renderizar las funciones es tomar los datos que se procesan y mostrarlos en pantalla y si hubiera alguna modificación de la misma manera.
+
+const renderPokemons = (pokemons = []) => {
+    const pokemonsList = document.querySelector('#pokemonList')
+
+    let elements = ''
+
+    // TODO: Necesitamos agregar una imagen a cada pokemon
+
+    pokemons.forEach(pokemon => {
+        elements += `
+            <article class="pokemon-item">
+                <h2>${pokemon.name}</h2>
+                <img
+                    src="${pokemon.image}"
+                    width="80"
+                    height="80"
+                />
+                <div class="pokemon-item__buttons">
+                    <button>
+                        <img src="images/icon-star.svg"  width="16"/>
+                    </button>
+                                                          <button>
+                        <img src="images/icon-edit.svg"  width="16"/>
+                    </button>
+                </div>
+            </article>
+        
+        `
+    })
+
+    pokemonsList.innerHTML = elements
+
+}
+
+fetchPokemons()
+    .then(data => {
+        console.log (data.results)
+        renderPokemons(data.results)
+    })
